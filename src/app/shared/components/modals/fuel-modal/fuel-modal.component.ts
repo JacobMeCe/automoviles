85ee3906b594970a934b7ee8bc3ebe8d884ef657
JonathanVidrio <cepo.sextino@outlook.com>
2024-02-07 17:17:15 -0600

    create modals components

---
+++ src/app/shared/components/modals/fuel-modal/fuel-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fuel-modal',
+  templateUrl: './fuel-modal.component.html',
+  styleUrls: ['./fuel-modal.component.scss']
+})
+export class FuelModalComponent {
+
+}
